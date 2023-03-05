df30e40743173392a711b399a213afc8fefbf88c
Kiran Shinde <105917619+8623924432@users.noreply.github.com>
2023-03-05 12:34:35 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'nipamTechnology';
+    constructor() { }
+
+    modalshow:boolean=false;
+    show(){
+        this.modalshow=!this.modalshow;
+    }
+
+    profileShow:boolean=false;
+
+    pshow()
+    {
+      this.profileShow=!this.profileShow;
+
+    }
+    
+  
+}
